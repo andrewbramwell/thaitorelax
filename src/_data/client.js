33446ca8772564c19dbdb26616f73135e1feb30f
andrewbramwell <97716059+andrewbramwell@users.noreply.github.com>
2025-10-10 14:51:43 +0100

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "name": "Splendid Sites Web Designs",
+    "email": "help@splendidsites.com",
+    "phoneForTel": "555-779-4407",
+    "phoneFormatted": "(555) 779-4407",
+    "openingHours": "Mon-Sun: 9am-9pm",
+    "onlineBooking": "https://www.facebook.com/",
+    "address": {
+        "lineOne": "First Address Line",
+        "lineTwo": "Second Address Line",
+        "city": "Denver",
+        "state": "CO",
+        "zip": "80206",
+        "country": "US",
+        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+        "googleReview": "",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.example.com"
+};
