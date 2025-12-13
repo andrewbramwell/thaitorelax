694a7d57750bcf40da8397f949b50df771bbc1fa
“andrewbramwell <andy@voucherstore.io>
2025-12-13 21:10:19 +0700

    schema

--- src/_data/client.js
+++ src/_data/client.js
@@ -23,5 +23,48 @@ module.exports = {
         "googleReview": "https://g.page/r/CTFT40-oEVrlEBM/review",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://thaitorelaxleeds.co.uk"
+    "domain": "https://thaitorelaxleeds.co.uk",
+    "schema": {
+        "business": {
+            "@context": "https://schema.org",
+            "@type": "HealthAndBeautyBusiness",
+            "@id": "https://thaitorelaxleeds.co.uk/#business",
+            "name": "Thai To Relax",
+            "image": "https://thaitorelaxleeds.co.uk/assets/svgs/logo.svg",
+            "url": "https://thaitorelaxleeds.co.uk",
+            "telephone": "+447921223513",
+            "email": "thaitorelaxleeds@gmail.com",
+            "priceRange": "££",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Advance Hair Studio, Carlton Tower, 34 St Paul's Street",
+                "addressLocality": "Leeds",
+                "postalCode": "LS1 2QB",
+                "addressCountry": "GB"
+            },
+            "geo": {
+                "@type": "GeoCoordinates",
+                "url": "https://maps.app.goo.gl/zX8tZxbGTxuzKWxc7"
+            },
+            "openingHoursSpecification": {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+                "opens": "10:00",
+                "closes": "18:00"
+            },
+            "sameAs": [
+                "https://g.page/r/CTFT40-oEVrlEBM/review"
+            ]
+        },
+        "website": {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "@id": "https://thaitorelaxleeds.co.uk/#website",
+            "url": "https://thaitorelaxleeds.co.uk",
+            "name": "Thai To Relax",
+            "publisher": {
+                "@id": "https://thaitorelaxleeds.co.uk/#business"
+            }
+        }
+    }
 };
